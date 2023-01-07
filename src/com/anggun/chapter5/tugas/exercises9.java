f58493dcdf949f3958527dc0ef1bb63b9546291b
ITC_KAMPUS <anggungustina17@gmail.com>
2023-01-07 13:32:51 +0700

    add chapter3

--- src/com/anggun/chapter5/tugas/exercises9.java
+++ src/com/anggun/chapter5/tugas/exercises9.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class exercises9 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.print("enter the number of students:  ");
+        System.out.print("Enter the number of students:  ");
         int numberOfStudents = input.nextInt();
         int score, highest = 0, secondHigest = 0;
         String name = " ", studen1 = " ", student2 = " ";
+//        student1 = " ";
     }
 }
