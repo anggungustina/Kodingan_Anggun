6d7dc38b43ee32f65163c67de55fea41fb4ec9a6
ITC_KAMPUS <anggungustina17@gmail.com>
2023-01-11 10:29:59 +0700

    fix no 7 and 17

--- src/com/anggun/chapter5/tugas/exercises7.java
+++ src/com/anggun/chapter5/tugas/exercises7.java
@@ -1,16 +1,16 @@
-//package com.anggun.chapter5.tugas;
-//
-//public class exercises7 {
-//    public static void main(String[] args) {
-//        int totalCost = 0;
-//        int tuition = 10000;
-//        int tuitionTenthYear;
-//        for (int year = 1; year <= 14; year++) {
-//            tuition += (tuition * 0.05);
-//            if (year > 10) totalCost +=tuition;
-//            if (year == 10) tuitionTenthYear = tuition;
-//        }
-//        System.out.println("Tuition in ten years: $" + tuitionTenthYear);
-//        System.out.println("Total cost for four years' worth of tuition" + " after the tenthyear: $" + totalCost);
-//    }
-//}
+package com.anggun.chapter5.tugas;
+
+public class exercises7 {
+    public static void main(String[] args) {
+        int totalCost = 0;
+        int tuition = 10000;
+        int tuitionTenthYear = 0;
+        for (int year = 1; year <= 14; year++) {
+            tuition += (tuition * 0.05);
+            if (year > 10) totalCost += tuition;
+            if (year == 10) tuitionTenthYear = tuition;
+        }
+        System.out.println("Tuition in ten years: $" + tuitionTenthYear);
+        System.out.println("Total cost for four years' worth of tuition" + " after the tenthyear: $" + totalCost);
+    }
+}
