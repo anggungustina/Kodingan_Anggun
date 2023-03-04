011a8ec5c55372283c10f2dc0821d96cfd2a8a81
LAB 2B-05 <anggungustina17@gmail.com>
2023-03-03 16:29:04 -0800

    3/3/23

---
+++ src/com/anggun/chapter10/tugas/Account.java
@@ -0,0 +1,52 @@
+package com.anggun.chapter10.tugas;
+import  java.util.Date;
+public class Account {
+    private int id;
+    private double balance;
+    private static double annualInterestRate;
+    private Date dateCreated;
+
+    Account() {
+        id = 0;
+        balance = 0;
+        annualInterestRate = 0;
+        dateCreated = new Date();
+    }
+    Account(int newId, double newBalance) {
+        id = newId;
+        balance = newBalance;
+        dateCreated = new Date();
+    }
+    public void setId(int newId) {
+        id = newId;
+    }
+    public void setBalance(double newbalance){
+        balance = newbalance;
+    }
+    public void setAnnualInterestRate (double newAnnualInterestRate) {
+        annualInterestRate = newAnnualInterestRate;
+    }
+    public int getId() {
+        return  id;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public double getAnnualInterestRate() {
+        return  annualInterestRate;
+    }
+    public String getDateCreated() {
+        return dateCreated.toString();
+    }
+    public  double getMonthlyInterestRate() {
+        return balance * (getMonthlyInterestRate() / 100);
+
+    }
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+}
--- src/com/anggun/chapter10/tugas/Ex5_Displaytheprimefactors.java
+++ src/com/anggun/chapter10/tugas/Ex5_Displaytheprimefactors.java
@@ -1,7 +1,34 @@
 package com.anggun.chapter10.tugas;
 
+import java.util.Scanner;
+
 public class Ex5_Displaytheprimefactors {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        System.out.println("The smallest factors of " + number + " are: ");
+        StackOfInteger stack = new StackOfInteger(4); //ngarang aja laa
+
+        smallestFactors(number, stack);
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+
+        }
+        System.out.println();
+
+    }
+    public static void smallestFactors(int number, StackOfInteger stack) {
+        int i = 2;
+        while ((number / i != 1)) {
+            if (number % i == 0) {
+                stack.push(i);
+                number /= i;
+            } else
+                i++;
+        }
+        stack.push(number);
     }
 }
